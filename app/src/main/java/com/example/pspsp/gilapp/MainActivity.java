3fb6347474e7dbd2ca5280c17aec8c2e62d3ce75
pspsp456 <pspsp456@gmail.com>
2019-01-14 15:37:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pspsp/gilapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.pspsp.gilapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.ExerciseList);
+
+        List<String> data = new ArrayList<String>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+
+        listView.setAdapter(adapter);
+
+        data.add("금일 현재까지 먹은 칼로리 수치는" + 500 + "kcal 입니다.");
+        data.add("현재 부위별 운동 빈도");
+        data.add("체중");
+        data.add("지방");
+        data.add("골격근량");
+
+    }
+
+
+}
