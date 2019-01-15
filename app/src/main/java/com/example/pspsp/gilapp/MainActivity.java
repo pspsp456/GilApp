43368f804bf500c0646462ee562227722613741a
pspsp456 <pspsp456@gmail.com>
2019-01-15 14:19:25 +0900

    날짜 관련 연동과 식단 기록 해서 나오는 것 까지 했음

--- app/src/main/java/com/example/pspsp/gilapp/MainActivity.java
+++ app/src/main/java/com/example/pspsp/gilapp/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.pspsp.gilapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
-    String kcal = MealManagerActivity.GetKcal();
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+
         listView = (ListView)findViewById(R.id.ExerciseList);
+        textView = (TextView)findViewById(R.id.MainText);
+
+        textView.setText("당신은 운동" + dayCount() + "일 차입니다");
+
 
         List<String> data = new ArrayList<String>();
 
@@ -26,13 +43,22 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
-        data.add("금일 현재까지 먹은 칼로리 수치는" + kcal + "kcal 입니다.");
+        data.add("금일 현재까지 먹은 칼로리 수치는" + intent.getStringExtra("kcal") + "kcal 입니다.");
         data.add("현재 부위별 운동 빈도");
-        data.add("체중");
-        data.add("지방");
-        data.add("골격근량");
+        data.add("체중: " + intent.getStringExtra("kg"));
+        data.add("지방: " + intent.getStringExtra("fat"));
+        data.add("골격근량: " + intent.getStringExtra("muscle"));
 
     }
 
+    private long dayCount()
+    {
+        long firstTime = 0;
+        long time = System.currentTimeMillis();
+
+        time = (time - firstTime) /(1000 * 3600 * 24 * 365);
+
+        return time;
+    }
 
 }
--- app/src/main/java/com/example/pspsp/gilapp/MealManagerActivity.java
+++ app/src/main/java/com/example/pspsp/gilapp/MealManagerActivity.java
@@ -3,14 +3,18 @@ package com.example.pspsp.gilapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MealManagerActivity extends AppCompatActivity {
 
-    static public String kcal;
     EditText kcalText;
+    EditText kgText;
+    EditText fatText;
+    EditText muscleText;
+
     Button kcalButton;
 
     @Override
@@ -19,19 +23,24 @@ public class MealManagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meal_manager);
 
         kcalText = (EditText)findViewById(R.id.foodKcal);
+        kgText = (EditText)findViewById(R.id.kgTextID);
+        fatText = (EditText)findViewById(R.id.fatTextID);
+        muscleText = (EditText)findViewById(R.id.muscleTextID);
+
         kcalButton = (Button)findViewById(R.id.kcalButton);
     }
 
     public void kcalOnClick(View v)
     {
-        kcal = kcalText.getText().toString();
-
         Intent mainIntent = new Intent(MealManagerActivity.this, MainActivity.class);
+
+        Log.d(kcalText.getText().toString(), "값이 어떻게 나오니?");
+        mainIntent.putExtra("kcal", kcalText.getText().toString());
+        mainIntent.putExtra("kg", kgText.getText().toString());
+        mainIntent.putExtra("fat", fatText.getText().toString());
+        mainIntent.putExtra("muscle", muscleText.getText().toString());
+
         MealManagerActivity.this.startActivity(mainIntent);
     }
 
-    static public String GetKcal()
-    {
-        return kcal;
-    }
 }
